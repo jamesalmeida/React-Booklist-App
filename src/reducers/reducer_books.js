b536545f972585c78f1effdc070ccbe32fe11c79
James Almeida <jimmyplaysdrums@gmail.com>
2018-07-19 13:15:48 -0700

    added reducers and actions for Redux.

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function () {
   return [
-    { title: 'Javascript: the Good Parts' },
-    { title: 'Harry Potter '},
-    { title: 'Dark Tower' },
-    { title: 'Eloquent Ruby' },
+    { title: 'Javascript for Dummies', pages: 323 },
+    { title: 'The Adventures of Sherlock Holmes', pages: 103 },
+    { title: 'A Game of Thrones', pages: 489 },
+    { title: 'Think Like A Programmer', pages: 298 },
   ]
 }
